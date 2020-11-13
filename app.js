4faffe3418eb6ea7662d53b1ac6cdf76325c1f3c
anarubado <rubadoana@gmail.com>
2020-11-13 00:47:27 -0300

    Upd

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require("express"); // Requerimos la gran función de Express 
+const app = express(); // Ejecutamos esta función la cual devolverá un objeto literal con muchos métodos y atributos
 
-const apiInfoRouter = require('./routes/api/apiInfo');
+const cors = require("cors");
+app.use(cors());
+
+const apiInfoRouter = require('./routes/api/apiInfo'); // Requerimos el router
 
 app.listen(3030, function(){
-    console.log("Running on 3030");
+    console.log("Running on 3030"); // Express levantará un servidor y éste escuchará en el puerto 3030
 });
 
-app.use('/api/info', apiInfoRouter);
+
+app.use('/api/info', apiInfoRouter); // Asignamos que la ruta "/api/info" apunte a nuestro router particular que requerimos
